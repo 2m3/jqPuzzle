04120dc200bd5b93f096309922ba10bb0470aa1b
Ralf Stoltze <ralf.stoltze@2meter3.de>
2013-10-20 16:05:07 +0200

    moved static initializations out of beforeEach()

--- test/jasmine/spec/core.helper.js
+++ test/jasmine/spec/core.helper.js
@@ -1,9 +1,6 @@
 describe("Helper: ", function() {
 
 	var puzzle;
-	var board3x3;
-	var positions3x3;
-	var pieces3x3;
 
 	var ROW_INVALID			= 'invalid row';
 	var COL_INVALID			= 'invalid col';
@@ -11,38 +8,36 @@ describe("Helper: ", function() {
 	var NUMBER_INVALID		= 'invalid number';
 	var DIRECTION_INVALID	= 'invalid direction';
 
-	beforeEach(function() {
-		// a 3x3 board with 0 replaced for 9, solved
-		board3x3 = [1,	2,	3,
+	// a 3x3 board with 0 replaced for 9, solved
+	var board3x3 = [1,	2,	3,
 					4,	5,	6,
 					7,	8,	0];
 
-		// positions on a 3x3 board
-		positions3x3 = {
-			topLeft:		{ index: 1, row: 1, col: 1 },
-			topMiddle:		{ index: 2, row: 1, col: 2 },
-			topRight:		{ index: 3, row: 1, col: 3 },
-			middleLeft:		{ index: 4, row: 2, col: 1 },
-			middleMiddle:	{ index: 5, row: 2, col: 2 },
-			middleRight:	{ index: 6, row: 2, col: 3 },
-			bottomLeft:		{ index: 7, row: 3, col: 1 },
-			bottomMiddle:	{ index: 8, row: 3, col: 2 },
-			bottomRight:	{ index: 9, row: 3, col: 3 }
-		};
+	// positions on a 3x3 board
+	var positions3x3 = {
+		topLeft:		{ index: 1, row: 1, col: 1 },
+		topMiddle:		{ index: 2, row: 1, col: 2 },
+		topRight:		{ index: 3, row: 1, col: 3 },
+		middleLeft:		{ index: 4, row: 2, col: 1 },
+		middleMiddle:	{ index: 5, row: 2, col: 2 },
+		middleRight:	{ index: 6, row: 2, col: 3 },
+		bottomLeft:		{ index: 7, row: 3, col: 1 },
+		bottomMiddle:	{ index: 8, row: 3, col: 2 },
+		bottomRight:	{ index: 9, row: 3, col: 3 }
+	};
 
-		// positions on a 3x3 board
-		pieces3x3 = {
-			topLeft:		{ number: 1, position: positions3x3.topLeft },
-			topMiddle:		{ number: 2, position: positions3x3.topMiddle },
-			topRight:		{ number: 3, position: positions3x3.topRight },
-			middleLeft:		{ number: 4, position: positions3x3.middleLeft },
-			middleMiddle:	{ number: 5, position: positions3x3.middleMiddle },
-			middleRight:	{ number: 6, position: positions3x3.middleRight },
-			bottomLeft:		{ number: 7, position: positions3x3.bottomLeft },
-			bottomMiddle:	{ number: 8, position: positions3x3.bottomMiddle },
-			bottomRight:	{ number: 0, position: positions3x3.bottomRight }
-		};
-	});
+	// positions on a 3x3 board
+	var pieces3x3 = {
+		topLeft:		{ number: 1, position: positions3x3.topLeft },
+		topMiddle:		{ number: 2, position: positions3x3.topMiddle },
+		topRight:		{ number: 3, position: positions3x3.topRight },
+		middleLeft:		{ number: 4, position: positions3x3.middleLeft },
+		middleMiddle:	{ number: 5, position: positions3x3.middleMiddle },
+		middleRight:	{ number: 6, position: positions3x3.middleRight },
+		bottomLeft:		{ number: 7, position: positions3x3.bottomLeft },
+		bottomMiddle:	{ number: 8, position: positions3x3.bottomMiddle },
+		bottomRight:	{ number: 0, position: positions3x3.bottomRight }
+	};
 
 	describe("Solving: ", function() {
 
--- test/jasmine/spec/core.init.js
+++ test/jasmine/spec/core.init.js
@@ -1,33 +1,28 @@
 describe("Initialization:", function() {
 
 	var puzzle;
-	var board3x3;
-	var board4x4;
-	var board2x5;
 
-	beforeEach(function() {
-		// 0 replaced for 4, unsolvable
-		board2x2 = [3,	2,
+	// 0 replaced for 4, unsolvable
+	var board2x2 = [3,	2,
 					1,	0];
 
-		// 0 replaced for 9, solved
-		board3x3 = [1,	2,	3,
+	// 0 replaced for 9, solved
+	var board3x3 = [1,	2,	3,
 					4,	5,	6,
 					7,	8,	0];
 
-		// 0 replaced for 16, different initial and solved hole positions
-		board4x4 = [2,	4,	6,	8,
+	// 0 replaced for 16, different initial and solved hole positions
+	var board4x4 = [2,	4,	6,	8,
 					10,	12,	14,	0,
 					1,	3,	5,	7,
 					9,	11,	13,	15];
 
-		// 0 replaced for 10, different initial and solved hole positions
-		board2x5 = [0,	9,
+	// 0 replaced for 10, different initial and solved hole positions
+	var board2x5 = [0,	9,
 					8,	7,
 					6,	5,
 					4,	3,
 					2,	1];
-	});
 
 	describe("When initialized, a slider puzzle", function() {
 
